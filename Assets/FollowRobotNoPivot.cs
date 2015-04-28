bfd1207b8c8dfbc4e8f2c711233dc6ed08bb7ce9
daylannance <daylannance@gmail.com>
2015-04-28 02:49:46 -0400

    Adds first bot model and some terrain.

---
+++ Assets/FollowRobotNoPivot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowRobotNoPivot : MonoBehaviour {
+	public Transform target;
+	public Vector3 offset;
+	bool isOffsetInitialized = false;
+	// Use this for initialization
+	void Awake () {
+		offset = target.position - transform.position;
+		isOffsetInitialized = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isOffsetInitialized)
+		{
+			transform.position = target.position - offset;
+		}
+	}
+}
---
+++ Assets/TorqueBody.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorqueBody : MonoBehaviour {
+	public float turningTorque;
+	public ForceMode turningForceMode;
+	public float movingForce;
+	public ForceMode movingForceMode;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			GetComponent<Rigidbody>().AddTorque(0, turningTorque,0,turningForceMode);
+		}
+		if(Input.GetKey (KeyCode.RightArrow))
+		{
+			GetComponent<Rigidbody>().AddTorque(0, -turningTorque,0,turningForceMode);
+		}
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(transform.up * movingForce,movingForceMode);
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			GetComponent<Rigidbody>().AddForce(transform.up * -movingForce,movingForceMode);
+		}
+	}
+}
