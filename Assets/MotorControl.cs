177af84b3dcfaaff7fc6f712da4f206c704b6428
daylannance <daylannance@gmail.com>
2015-05-01 13:24:47 -0400

    Adds PincherBot

---
+++ Assets/MotorControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotorControl : MonoBehaviour {
+	
+	public float force;
+	public float targetSpeed;
+	HingeJoint joint;
+	// Use this for initialization
+	void Start () {
+		joint = GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		JointMotor motor = new JointMotor();
+		joint.useMotor = true;
+		if(Input.GetKey (KeyCode.C))
+		{
+			motor.force = force;
+			motor.targetVelocity = targetSpeed;
+		}
+		else if(Input.GetKey (KeyCode.O))
+		{
+			motor.force = -force;
+			motor.targetVelocity = -targetSpeed;
+		}
+		else
+		{
+			motor.force = 0;
+			motor.targetVelocity=0;
+			motor.freeSpin = false;
+		}
+		joint.motor = motor;
+	}
+}
---
+++ Assets/UpDownMotorControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDownMotorControl : MonoBehaviour {
+
+	public float force;
+	public float targetSpeed;
+	HingeJoint joint;
+	// Use this for initialization
+	void Start () {
+		joint = GetComponent<HingeJoint>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		JointMotor motor = new JointMotor();
+		joint.useMotor = true;
+		if(Input.GetKey (KeyCode.L))
+		{
+			motor.force = force;
+			motor.targetVelocity = targetSpeed;
+		}
+		else if(Input.GetKey (KeyCode.R))
+		{
+			motor.force = -force;
+			motor.targetVelocity = -targetSpeed;
+		}
+		else
+		{
+			motor.force = 0;
+			motor.targetVelocity=0;
+			motor.freeSpin = false;
+		}
+		joint.motor = motor;
+	}
+}
